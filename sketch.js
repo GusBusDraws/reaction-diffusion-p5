73685dd04815531218694816e9df176464874e55
Gus Becker <60982210+GusBusDraws@users.noreply.github.com>
2025-02-16 15:09:29 -0500

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,15 @@
+// @ts-check
+/// <reference path="./node_modules/@types/p5/global.d.ts" />
+function setup() {
+  createCanvas(400, 400)
+  resetSketch()
+  console.log('Press SPACE to stop looping or r to reset.')
+}
+
+function draw() {
+  background(0)
+}
+
+function resetSketch() {
+  background(0)
+}
